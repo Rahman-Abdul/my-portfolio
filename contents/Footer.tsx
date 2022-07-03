2f83f8cfcbeb47265af914578b968bc66223d63f
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2022-07-03 10:25:02 +0100

    some chages in css , home page and footer

--- contents/Footer.tsx
+++ contents/Footer.tsx
@@ -1,11 +1,27 @@
+import Image from 'next/image'
+import github from '../assets/github.png'
+import linkedin from '../assets/linkedin.png'
+import twitter from '../assets/twitter.png'
+import instagram from '../assets/instagram.png'
+
+
 interface Props {
     
 }
 
 const Footer = (props: Props) => {
     return (
-        <section>
-            Copyright @2022
+        <section className='footer-section'>
+            <p className='sign'>abdul_rahman ahmed</p>
+            <div className='footer-social-section'>
+          <Image className='social-image' src={twitter} alt="Picture of the author" width={50.03} height={50.03}/>
+        <Image className='social-image' src={github} alt="Picture of the author" width={50.03} height={50.03}/>
+      <Image className='social-image' src={linkedin} alt="Picture of the author" width={50.03} height={50.03}/>
+      <Image className='social-image' src={instagram} alt="Picture of the author" width={50.03} height={50.03}/>
+          </div>
+          <div>
+        <p>Copyright @2022 </p>
+        </div>
         </section>
     )
 }
