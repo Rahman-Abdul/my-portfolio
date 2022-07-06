d4a0a140023ea974a6d0e87eaa6168b7c1eaa926
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2022-07-06 13:37:55 +0100

    initial commits

--- contents/Footer.tsx
+++ contents/Footer.tsx
@@ -19,7 +19,7 @@ const Footer = (props: Props) => {
           <Image className='social-image foot' src={twitter} alt="Picture of the author" width={50.03} height={50.03}/>
         <Image className='social-image foot' src={github} alt="Picture of the author" width={50.03} height={50.03}/>
       <Image className='social-image foot' src={linkedin} alt="Picture of the author" width={50.03} height={50.03}/>
-      <Image className='social-image foot' src={instagram} alt="Picture of the author" width={50.03} height={50.03}/>
+      <Image className='social-image footnpm' src={instagram} alt="Picture of the author" width={50.03} height={50.03}/>
           </div>
           <div className='copy-section'>
         <h3 className='copy'>Copyright @2022</h3>
