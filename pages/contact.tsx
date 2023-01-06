dbcde9db9e88a040609b538ea6cea920c17b3e20
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2023-01-06 22:37:19 +0100

    index page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,47 +2,8 @@ import React from 'react';
 import { useState } from 'react';
 
 const Contact: any = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+ <div></div>
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    const data = {
-      name,
-      email,
-      message,
-    };
-    console.log(data);
-  };
-
-  return (
-      
-      <main>
-        <form onSubmit={handleSubmit} className="form">
-          <label htmlFor="name">Name:</label>
-          <input
-            id="name"
-            type="text"
-            onChange={e => setName(e.target.value)}
-          />
-          <label htmlFor="email">Email:</label>
-          <input
-            id="email"
-            type="email"
-            onChange={e => setEmail(e.target.value)}
-          />
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            type="text"
-            rows="4"
-            onChange={e => setMessage(e.target.value)}
-          />
-          <button type="submit">Send</button>
-        </form>
-      </main>
-  );
 }
 
 export default Contact;
