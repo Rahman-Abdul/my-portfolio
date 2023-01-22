a37f7995b53a61f820c1a6e8c91c30cb54829d17
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2023-01-22 22:40:04 +0200

    new changes in the in work section

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,7 +2,26 @@ import React from 'react';
 import { useState } from 'react';
 
 const Contact: any = () => {
- <div></div>
+
+    return(
+        <div className={styles.container}>
+  < form className="smain" >
+  <formGroup className="inputGroup" >
+    < label htmlFor='name'>Name</label>
+    < input type='text' name='name' className={styles.inputField} />
+  </formGroup>
+  < formGroup className={styles.inputGroup} >
+    < label htmlFor='email'>Email</label>
+    < input type='email' name='email' className={styles.inputField} />
+  </formGroup>
+  < formGroup className={styles.inputGroup} >
+    < label htmlFor='message'>Message</label>
+    < input type='text' name='message' className={styles.inputField} />
+  </formGroup>
+  < input type='submit'/>
+  </form >
+</div>
+    )
 
 }
 
