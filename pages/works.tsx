0322698167e0ae3904814df53440899a0ba3f211
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2023-01-26 20:11:05 +0100

    new changes

--- pages/works.tsx
+++ pages/works.tsx
@@ -39,7 +39,7 @@ const Works: any = () => {
     <a target="_blank" href="img_mountains.jpg">
     <Image src={html} alt="Illustrator" width="600" height="400"/>
     </a>
-    <div className="desc">Add a description of the image here</div>
+    <div className="desc">manduu website</div>
   </div>
 </div>
 
