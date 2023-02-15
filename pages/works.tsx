7a42c21b8b7184f4ac320716e6f553ff9212a619
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2023-02-15 08:27:16 +0100

    removed packag json

--- pages/works.tsx
+++ pages/works.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
 import html from '../assets/html.png';
-import angularDashboard from '../assets/angularDashboard.png'
-import doctor from '../assets/doctor.png'
-import table from '../assets/table.png'
-import portfolio from '../assets/portfolio.png'
-import symphony from '../assets/symphony.png'
-import texas from '../assets/texas.png'
-import hp from '../assets/hp.png'
+import angularDashboard from '../assets/angularDashboard.png';
+import doctor from '../assets/doctor.png';
+import table from '../assets/table.png';
+import portfolio from '../assets/portfolio.png';
+import symphony from '../assets/symphony.png';
+import texas from '../assets/texas.png';
+import hp from '../assets/hp.png';
 
 
 // import { Container } from './styles';
