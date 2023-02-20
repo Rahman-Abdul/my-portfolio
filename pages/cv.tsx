da716617f4e59f5a94cd427e9b160eac79b03a59
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2023-02-20 23:09:19 +0100

    new lock

---
+++ pages/cv.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+
+
+
+const Cv = () => {
+	const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }: any) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Document file="ahmed.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
+  );
+}
+
+export default Cv;
