0e8d833076f4449368de0591814134ed8d2cb897
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2023-02-22 01:59:44 +0100

    new lock

--- contents/contactDetails.tsx
+++ contents/contactDetails.tsx
@@ -12,7 +12,7 @@ export default function Contactdetails({}: Props) {
     <div className='contact'>
       <ul>
         <li className='social'><a href={'tel:+2347036761504'} target={"_blank"} rel={"noreferrer"}><Image className='social-image' src={phone} alt="Picture of the author" width={36.03} height={36.03}/> <span className='details'>+2347036761504</span></a> </li>
-        <li className='social'><a href={'mailto:abdulrahmanahmedrasheed@gmail.com'} target={"_blank"} rel={"noreferrer"}><Image className='social-image' src={mail} alt="Picture of the author" width={36.03} height={36.03}/><span className='details'>abdulrahmanahmedrasheed@gmail.com</span></a></li>
+        <li className='social'><a href={'mailto:abdulrahmanahmedrasheed@gmail.com'} target={"_blank"} rel={"noreferrer"}><Image className='social-image' src={mail} alt="Picture of the author" width={36.03} height={36.03}/><span className='details'>Abdulrahman Ahmed</span></a></li>
         <li className='social'><a href={'https://www.linkedin.com/in/abdulrahman-ahmed-538a05178/'} target={"_blank"} rel={"noreferrer"}><Image className='social-image details' src={linkedin} alt="Picture of the author" width={36.03} height={36.03}/><span className='details'>Abdulrahman Ahmed</span></a></li>
       </ul>
           </div>
