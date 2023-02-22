d4818580117836665a1c1be5d47844e1b2d2dd37
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2023-02-22 15:22:46 +0100

    new lock

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import me from '../assets/me.jpg';
+import mee from '../assets/mee.jpg';
 
 
 
@@ -10,7 +10,7 @@ const About: any = () => {
 return(
 <main className=''>
 <div className='circle-parent circle'>
-  <Image className='about-image' src={me} alt="Picture of the website"/>
+  <Image className='about-image' src={mee} alt="Picture of the website"/>
   </div>
 <p className='about-p'>Dedicated and hardworking software engineer with a flair for creating elegant solutions in the least amount of time. Developed and design an ecommerce, webapp, customers web portal, documentary, launch websites, and donations webapps for a local charity. Passionate about software architecture and cloud computing. Regular attendee of web and application developer meetups and github, technically talented with the ability to learn and understand faster.</p>
 
