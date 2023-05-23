69832250bf01c1537e18e990b1ffba7638f636fd
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2023-05-23 07:32:04 +0100

    new changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,13 +8,13 @@ const About: any = () => {
 
 
 return(
-<main className=''>
+<div className='about_me'>
 <div className='circle-parent circle'>
   <Image className='about-image' src="/me.png" width={400} height={400} alt="Picture of the website"/>
   </div>
 <p className='about-p'>Dedicated and hardworking software engineer with a flair for creating elegant solutions in the least amount of time. Developed and design an ecommerce, webapp, customers web portal, documentary, launch websites, and donations webapps for a local charity. Passionate about software architecture and cloud computing. Regular attendee of web and application developer meetups and github, technically talented with the ability to learn and understand faster.</p>
 
-</main>
+</div>
   )
 }
 
